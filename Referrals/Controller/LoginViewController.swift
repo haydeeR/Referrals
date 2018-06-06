2a4138df0b3e721da55e89db3368bdceec873f06
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-06-06 11:53:40 -0500

    Initial commit: Add gitignore, readme and the project base with login view in different storyboard

---
+++ Referrals/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Referrals
+//
+//  Created by Haydee Rodriguez on 6/6/18.
+//  Copyright © 2018 Haydee Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
