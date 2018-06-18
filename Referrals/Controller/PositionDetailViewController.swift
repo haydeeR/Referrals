ac43bcfb64e9b73da30194ed2e871984abc3ba01
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-06-18 11:52:37 -0500

    Position view with the table view, segment linkedin-contact view and one container

---
+++ Referrals/Controller/PositionDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PositionDetailViewController.swift
+//  Referrals
+//
+//  Created by Haydee Rodriguez on 6/18/18.
+//  Copyright © 2018 Haydee Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PositionDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var typeOfViewSegment: UISegmentedControl!
+    @IBOutlet weak var linkedInContainer: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
